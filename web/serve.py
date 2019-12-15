76f9ae88f3e57aab9ef1bdc8841d9e94d5e182ab
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-12-15 17:02:36 +0100

    creating file upload functionality and secure filename check

--- web/serve.py
+++ web/serve.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["FILE_UPLOADS"] = "/Users/kevin/desktop/Github/codeEval/tmp"
+app.config["ALLOWED_EXTENSIONS"] = ["py", "ipynb"]
 db = SQLAlchemy(app)
 
 
@@ -15,12 +18,28 @@ class User(db.Model):
     name = db.Column(db.String(200))
 
 db.create_all()
-example_user = User(id=1, name="Philip Sterne")
+example_user = User(name="Philip Sterne")
 db.session.merge(example_user)
 db.session.commit()
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
+
+    def is_valid(filename):
+        if filename and '.' in filename and filename.split('.')[1] in app.config["ALLOWED_EXTENSIONS"]:
+            return True
+        return False
+
+    if request.method == "POST":
+        if request.files:
+            file = request.files["code"]
+
+            if is_valid(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config["FILE_UPLOADS"], filename))
+
+            return redirect(request.url)
+
     return render_template('index.html')
 
 @app.route('/users')
