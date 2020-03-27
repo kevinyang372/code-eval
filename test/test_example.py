c2117e44176e57a4783e2e98be70baa622446782
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2020-03-27 16:22:53 -0300

    add unittest for api endpoints

--- test/test_example.py
+++ test/test_example.py
@@ -1,6 +1,7 @@
 import os
 import io
 import sys
+import json
 import unittest
 
 os.environ["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
@@ -86,6 +87,27 @@ class FlaskTestCase(unittest.TestCase):
 
             response = self.app.post('/submit/1/1', data = dict(filename=(io.BytesIO(to_test.encode()), 'test.py')), follow_redirects=True)
             assert "Passed Test Cases: 3 / 3" not in str(response.data)
+
+    # test apis get score
+    def test_apis_get_score(self):
+        self.test_upload_file()
+
+        response = self.app.post('/apis/get_score/example_admin_user@gmail.com/1', data = json.dumps({"credentials":{"email":"example_admin_user@gmail.com", "password":"111"}}), content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+
+    # test apis get file
+    def test_apis_submit_file(self):
+        self.test_create_session()
+
+        to_test = ''
+        with open('sample.py', 'r') as file:
+            for line in file:
+                to_test += line
+
+        response = self.app.post('/apis/submit/1', data = dict(file=(io.BytesIO(to_test.encode()), 'test.py'), email="example_admin_user@gmail.com", password="111"))
+
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
