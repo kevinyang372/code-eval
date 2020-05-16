5f16ac5212b827ccda30af75b333a9181829293c
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2020-05-15 18:00:52 -0700

    apply gradient descent example

---
+++ ml_sample_test.py
@@ -0,0 +1,31 @@
+from base import BaseTest
+import numpy as np
+
+class TestCases(BaseTest):
+
+    def __init__(self, func):
+        super().__init__(func)
+
+        self.parameters = { 'gradient_descent': [] }
+        self.answers = { 'gradient_descent': [] }
+
+        for _ in range(5):
+            
+            x_data = 2 * np.random.rand(100, 1)
+            y_data = 4 + 3 * x_data + np.random.randn(100,1)
+
+            init_theta = np.random.randn(2, 1)
+            learning_rate = 0.01
+            iteration = 500
+
+            comb = (x_data, y_data, init_theta, learning_rate, iteration)
+            self.parameters['gradient_descent'].append(comb)
+            self.answers['gradient_descent'].append(self.gradient_descent(*comb))
+
+
+    def gradient_descent(self, x, y, theta, learning_rate, iteration):
+        x = np.c_[np.ones((len(x), 1)), x]
+        for _ in range(iteration):
+            pred = np.dot(x,theta)
+            theta -= 1 / len(y) * learning_rate * (x.T.dot(pred - y))
+        return theta
--- web/base.py
+++ web/base.py
@@ -3,6 +3,7 @@ from RestrictedPython.PrintCollector import PrintCollector
 from RestrictedPython.Eval import default_guarded_getiter
 from RestrictedPython.Guards import guarded_iter_unpack_sequence
 
+import numpy as np
 from multiprocessing import Process, Queue
 import collections
 
@@ -35,12 +36,15 @@ class BaseTest(object):
                 errs[ind] = 'Failed to parse input'
             return errs
 
+        def _hook_getitem(obj, attr):
+            return obj.__getitem__(attr)
+
         safe_locals = {}
         safe_globals['_print_'] = PrintCollector
         safe_globals['_getiter_'] = default_guarded_getiter
         safe_globals['_iter_unpack_sequence_'] = guarded_iter_unpack_sequence
-        safe_globals.update(limited_builtins)
-        safe_globals.update(utility_builtins)
+        safe_globals['_getitem_'] = _hook_getitem
+        safe_globals['np'] = np
 
         for item in blacklist:
             if item in list(safe_globals.keys()):
@@ -70,8 +74,14 @@ class BaseTest(object):
                 errs[i] = "Time Out"
             else:
                 output = result.get()
+
                 if output[0] == 1:
                     errs[i] = output[1]
+                elif isinstance(output[1], (list, np.ndarray)):
+                    if all(output[1] == self.answers[entry_point][i]):
+                        errs[i] = "Passed"
+                    else:
+                        errs[i] = "Wrong Answers"
                 elif output[1] != self.answers[entry_point][i]:
                     errs[i] = "Wrong Answers"
                 else:
