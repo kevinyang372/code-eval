b3bea1986261c8d527778bde98d5b821bed21318
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2020-05-21 19:25:06 -0700

    add compare module

--- web/blueprints/compare.py
+++ web/blueprints/compare.py
@@ -5,7 +5,7 @@ from web.models import Result
 compare_template = Blueprint('compare', __name__, template_folder='../templates')
 
 
-@compare_template.route('/compare/<result_id1>/<result_id2>', methods=["GET", "POST"])
+@compare_template.route('/compare/<result_id1>/<result_id2>')
 @admin_required
 def compare(result_id1, result_id2):
 
@@ -13,3 +13,13 @@ def compare(result_id1, result_id2):
     r2 = Result.query.filter_by(id=result_id2).first()
 
     return render_template('compare.html', content=highlight_diff([r1.user.email, r2.user.email], [r1.content, r2.content]))
+
+
+@compare_template.route('/compare/<result_id1>')
+@admin_required
+def compare_index(result_id1):
+
+    r1 = Result.query.filter_by(id=result_id1).first()
+    results = Result.query.filter_by(session_id = r1.session_id)
+
+    return render_template('compare_index.html', results = results, r1 = r1.id)
