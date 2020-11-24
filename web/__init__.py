03b68f7bb5b8dab4f9ea36d7286c2e6349cfc2be
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2020-11-23 18:53:27 -0800

    remove sample user

--- web/__init__.py
+++ web/__init__.py
@@ -56,23 +56,23 @@ from web import models
 
 # add example user, seminar and session
 db.create_all()
-example_admin = models.User(
-    id=1, email="example_admin_user@gmail.com", is_admin=True)
-example_admin.set_password("111")
-db.session.merge(example_admin)
+# example_admin = models.User(
+#     id=1, email="example_admin_user@gmail.com", is_admin=True)
+# example_admin.set_password("111")
+# db.session.merge(example_admin)
 
-example_user = models.User(
-    id=2, email="example_user@gmail.com", is_admin=False)
-example_user.set_password("111")
-db.session.merge(example_user)
+# example_user = models.User(
+#     id=2, email="example_user@gmail.com", is_admin=False)
+# example_user.set_password("111")
+# db.session.merge(example_user)
 
-example_user = models.User(
-    id=3, email="example_user_2@gmail.com", is_admin=False)
-example_user.set_password("111")
-db.session.merge(example_user)
+# example_user = models.User(
+#     id=3, email="example_user_2@gmail.com", is_admin=False)
+# example_user.set_password("111")
+# db.session.merge(example_user)
 
-example_course = models.Course(id=1, course_num='CS156', registration="join156")
-db.session.merge(example_course)
+# example_course = models.Course(id=1, course_num='CS156', registration="join156")
+# db.session.merge(example_course)
 
 # example_session = models.Session(id = 1, session_num=1.1, seminar_id=1, entry_point="entry", runtime=1.0, blacklist='')
 # db.session.merge(example_session)
