229294b612baeaf4ddea62a507c29a7ea4920f0c
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2020-12-08 17:00:42 -0800

    modify sample.py to reflect using some builtin functions

--- sample.py
+++ sample.py
@@ -1,11 +1,16 @@
 # def entry(a, b):
 #     return math.floor(a + b)
 
+
 def add(a, b):
-    return a + b
+    for i in range(1, 10):
+        print(i)
+    return round(a + b)
+
 
 def subtract(a, b):
     return a - b
+
 
 def multiply(a, b):
     return a * b
