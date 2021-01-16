d8bcc751985b7fa4d57f729a1611105333cd2d52
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2021-01-15 17:15:51 -0800

    apply apted algorithm

---
+++ plagiarism/apted_tree_edit_distance.py
@@ -0,0 +1,59 @@
+import ast
+from apted import APTED, Config
+
+
+class Node:
+    def __init__(self, encoding):
+        self.value = encoding
+        self.children = []
+
+
+class CustomConfig(Config):
+    def rename(self, node1, node2):
+        """Compares attribute .value of trees"""
+        return 1 if node1.value != node2.value else 0
+
+    def children(self, node):
+        """Get left and right children of trees"""
+        return [x for x in node.children if x]
+
+
+def copyTree(node, dummy = None):
+    t = type(node).__name__
+
+    if 'name' in node._fields:
+        t += ':' + node.name
+    elif 'id' in node._fields:
+        t += ':' + node.id
+
+    curr = Node(t)
+    if dummy is not None:
+        dummy.children.append(curr)
+
+    for k, v in vars(node).items():
+        if isinstance(v, list):
+            for p in v:
+                if isinstance(p, ast.AST):
+                    copyTree(p, curr)
+        elif isinstance(v, ast.AST):
+            copyTree(v, curr)
+
+    return curr
+
+
+def getTreeSize(root):
+    return 1 + sum(getTreeSize(node) for node in root.children)
+
+
+if __name__ == '__main__':
+
+    with open("similar_code1.py", "r") as source:
+        tree1 = ast.parse(source.read())
+
+    with open("similar_code2.py", "r") as source:
+        tree2 = ast.parse(source.read())
+
+    r1, r2 = copyTree(tree1), copyTree(tree2)
+    apted = APTED(r1, r2, CustomConfig())
+
+    print(apted.compute_edit_distance() / max(getTreeSize(r1), getTreeSize(r2)))
